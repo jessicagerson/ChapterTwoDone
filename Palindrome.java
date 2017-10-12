3856356f21e272423bcdb1f7811981a3fbd8f0d1
jessicagerson <jessygerson123@gmail.com>
2017-10-12 12:50:52 -0700

    PalindromeCh2

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+
+public class Palindrome
+{
+    public String word;
+    private boolean isPalindrome;
+
+  
+    public Palindrome(String wd)
+    {
+        this.word = wd;
+        
+        
+    }
+    public boolean testWord(){
+        for (int i = 0; i <word.length(); i++){
+            System.out.println(word.substring(i, i+1));
+            //if (word.substring(i, i+1) == word.substring(
+        }
+        return isPalindrome;
+    
+    }
+}
